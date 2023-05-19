8ea6b90fe493997f5c4cefe5a213fc8398e8c741
Zin4759 <vnaaiu7614@outlook.kr>
2023-05-19 16:35:02 +0900

    Update website.js
    Signed-off-by: Zin4759 <vnaaiu7614@outlook.kr>

--- website.js
+++ website.js
@@ -1,11 +1,10 @@
 var http = require('http'),
-    fs = require('fs');
-
+  fs = require('fs');
 fs.readFile('./test.html', function (err, html) {
   if (err) {
     throw err;
   }
-  http.createServer( function(request, response) {
+  http.createServer(function(request, response) {
     response.writeHeader(200, {"Content-Type": "text/html"});
     response.write(html);
     response.end();
